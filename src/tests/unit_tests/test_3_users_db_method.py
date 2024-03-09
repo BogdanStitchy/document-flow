566bf8a10c20cc0263e220c22a466de55d763ecd
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-03-09 14:58:09 +0300

    a function for searching a document by period was written, a test for this function was written

--- src/tests/unit_tests/test_3_users_db_method.py
+++ src/tests/unit_tests/test_3_users_db_method.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 from pydantic import ValidationError
 import pytest
@@ -14,11 +14,11 @@ def database_user():
 
 
 @pytest.mark.parametrize("data, expected_exception", [
-    ((1, os.urandom(16), "doc1", "11111", "333333", datetime(2023, 7, 12), "docx"), None),
-    ((1, os.urandom(16), "doc2", "22222", "77777", datetime(2023, 7, 12), "pdf"), None),
-    ((1, os.urandom(16), "doc3", "55555", "99999", datetime(2023, 7, 12), "rtx"), None),
-    ((1, os.urandom(16), "doc3", "55555", 99999, datetime(2023, 7, 12), "rtx"), ValidationError),
-    ((1, os.urandom(16), "doc3", "55555", "99999", datetime(2023, 7, 12), 15.7), ValidationError)
+    ((1, os.urandom(16), "doc1", "11111", "333333", datetime.datetime(2019, 7, 12), "docx"), None),
+    ((1, os.urandom(16), "doc2", "22222", "77777", datetime.datetime(2020, 7, 12), "pdf"), None),
+    ((1, os.urandom(16), "doc3", "55555", "99999", datetime.datetime(2023, 7, 12), "rtx"), None),
+    ((1, os.urandom(16), "doc3", "55555", 99999, datetime.datetime(2023, 7, 12), "rtx"), ValidationError),
+    ((1, os.urandom(16), "doc3", "55555", "99999", datetime.datetime(2023, 7, 12), 15.7), ValidationError)
 ])
 def test_add_document(database_user, data, expected_exception):
     if expected_exception is None:
@@ -32,3 +32,18 @@ def test_get_document():
     from src.db.methods.admin_db_methods import AdminMethodsDB
     documents = AdminMethodsDB.get_all_documents()
     assert len(documents) == 3
+
+
+def test_good_find_document_period(database_user):
+    start_output_date = datetime.datetime.strptime("01.01.2019", "%d.%m.%Y")
+    end_output_date = datetime.datetime.strptime("01.01.2021", "%d.%m.%Y")
+    date_creating = datetime.date.today() + datetime.timedelta(days=1)
+
+    res = database_user.find_document_period(start_output_date, end_output_date,
+                                             start_output_date, date_creating)
+    assert len(res) == 2
+
+
+def test_broke_find_admins_period(database_user):
+    with pytest.raises(ValidationError):
+        database_user.find_document_period("01.05.2021", "01.05.2025", "01.05.2021", "01.05.2025")
--- src/tests/unit_tests/test_4_update_documents.py
+++ src/tests/unit_tests/test_4_update_documents.py
@@ -12,19 +12,12 @@ def database_admin():
 @pytest.mark.parametrize("id_document, kwargs, expected_exception", [
     (1, {"inner_number": "", "output_number": "", "output_date": "", "type_document": "", "name": ""}, None),
     (2, {"inner_number": "", "output_number": "", "output_date": "", "type_document": "", "name": ""}, None),
-    # (3, {"inner_number": "", "output_number": "", "output_date": "", "type_document": "", "name": ""}, None),
     (1, (123, 234), TypeError),
     (4, {"linner": "Test"}, ValueError)
 ])
 def test_update_document(database_admin, id_document, kwargs, expected_exception):
     if expected_exception is None:
-        database_admin.update_document(id_document, **kwargs)
-        # new_document = database_admin.get_one_user(id_user)
-        # for key, update_value in kwargs.items():
-        #     if update_value != "" and update_value is not None:
-        #         assert update_value == new_user[key]
-        #     else:
-        #         assert new_user[key] is not None and new_user[key] != ''
+        database_admin.edit_document(id_document, **kwargs)
     else:
         with pytest.raises(expected_exception):
-            database_admin.update_document(id_document, **kwargs)
+            database_admin.edit_document(id_document, **kwargs)

3072819fe97b6bedb3b706bcd541754dcbb789ac
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-03-23 14:56:18 +0300

    src/view/widget/widget_setting_departments.py

--- src/view/widget/widget_setting_departments.py
+++ src/view/widget/widget_setting_departments.py
@@ -235,8 +235,5 @@ if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
-    # MainWindow = QtWidgets.QMainWindow()
     ui = TreeHierarchy()
-    # ui.setupUi(MainWindow)
-    # MainWindow.show()
     sys.exit(app.exec_())

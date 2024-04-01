9c6adc5368ae4a1397e288c971042d6bfe30271b
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-04-01 14:07:19 +0300

    changed output of information about model instance

--- src/db/models/base.py
+++ src/db/models/base.py
@@ -6,3 +6,14 @@ from sqlalchemy.orm import as_declarative, mapped_column
 @as_declarative()
 class Base:
     id = mapped_column(Integer, autoincrement=True, primary_key=True)
+
+    repr_cols_num = 3
+    repr_cols = tuple()
+
+    def __repr__(self):
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"

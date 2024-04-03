e47f7b660cc1fb4399f36c65384fdaa50c379dda
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-04-03 15:25:53 +0300

    fixtures for adding data to db are supplemented with a check for table emptiness. These fixtures have been moved to the main conftest

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import sessionmaker
 
 from src.db.models import base
 from src.db.database_setup import set_new_engine
-
+from src.model.super_admin import SuperAdmin
+from src.model.administrator import Administrator
 from src.db.methods import super_admin_db_methods as sa_methods
 
-# data_base_local = "sqlite:///B:/работа/проекты с работы/document_flow/tests/testDB.db"
 data_base_memory = "sqlite:///:memory:"
 
 
@@ -27,6 +27,13 @@ def get_session(create_database):
     return session
 
 
+@pytest.fixture(scope="module")
+def db_methods_super_admin():
+    super_admin_methods = sa_methods.SuperAdminMethodsDB()
+    yield super_admin_methods
+
+
+# ------------------------------------for clean table DB---------------------------------------------------------------
 @pytest.fixture(scope="module")
 def clean_all_database(create_database, get_session):
     session = get_session
@@ -101,7 +108,57 @@ def clean_documents_table_database(create_database, get_session):
         session.close()
 
 
+# ------------------------------------for fill table DB---------------------------------------------------------------
 @pytest.fixture(scope="module")
-def db_methods_super_admin():
-    super_admin_methods = sa_methods.SuperAdminMethodsDB()
-    yield super_admin_methods
+def fill_db_admins(db_methods_super_admin):
+    existing_admins = db_methods_super_admin.get_all_admins()
+    if len(existing_admins) != 0:
+        print(f"Table admins not empty. existing_admins:\n{existing_admins}\n")
+        return
+
+    added_admins = [("Иванов", "Иван", "Иванович", "ivanadmin", "password1"),
+                    ("Петров", "Петр", "Петрович", "petradmin", "password2"),
+                    ("Сергеев", "Сергей", "Сергеевич", "sergeyadmin", "password3")]
+
+    for admin in added_admins:
+        SuperAdmin.add_admin(*admin)
+
+
+@pytest.fixture(scope="module")
+def fill_db_departments_and_departments_hierarchy(db_methods_super_admin):
+    existing_departments = db_methods_super_admin.get_all_departments()
+    if len(existing_departments) != 0:
+        print(f"Table departments not empty. existing_departments:\n{existing_departments}\n")
+        return
+
+    added_department = [
+        ("Финансовый", 101, None),  # id = 1
+        ("Кадровый", 102, 1),  # id = 2
+        ("Информационные Технологии", 103, 1),  # id = 3
+        ("Маркетинг", 104, 2),  # id = 4
+        ("Продажи", 105, 3)  # id = 5
+    ]
+    for department in added_department:
+        SuperAdmin.add_department(*department)
+
+
+@pytest.fixture(scope="module")
+def fill_db_users(create_database, fill_db_departments_and_departments_hierarchy, db_methods_super_admin):
+    existing_users = db_methods_super_admin.get_all_users()
+    if len(existing_users) != 0:
+        print(f"Table users not empty. existing_users:\n{existing_users}\n")
+        return
+
+    added_users = [("Иванов", "Иван", "Иванович", "ivanovii", "pass123", 1),
+                   ("Петрова", "Мария", "Алексеевна", "petrovama", "pass234", 2),
+                   ("Сидоров", "Николай", "Петрович", "sidorovnp", "pass345", 3),
+                   ("Кузнецов", "Алексей", "Дмитриевич", "kuznetsovad", "pass456", 4),
+                   ("Смирнова", "Екатерина", "Васильевна", "smirnovaev", "pass567", 5),
+                   ("Волков", "Дмитрий", "Анатольевич", "volkovda", "pass678", 4),
+                   ("Зайцева", "Ольга", "Геннадьевна", "zaytsevog", "pass789", 5)]
+    admin = Administrator()
+    admin.set_self_data({"id": 1, 'name': 'test', 'patronymic': 'test', 'last_name': 'test',
+                         'date_last_changes_password': "10.10.2023"},
+                        "login")  # потому что при добавлении пользователя, используется атрибут id админа
+    for user in added_users:
+        admin.add_user(*user)
--- src/tests/func_tests/conftest.py
+++ src/tests/func_tests/conftest.py
@@ -1,50 +1,5 @@
-from pathlib import Path
-
 import pytest
-from src.model.super_admin import SuperAdmin
-from src.model.administrator import Administrator
 from src.model.user import User
-
-
-@pytest.fixture(scope="module")
-def fill_db_admins(create_database):
-    added_admins = [("Иванов", "Иван", "Иванович", "ivanadmin", "password1"),
-                    ("Петров", "Петр", "Петрович", "petradmin", "password2"),
-                    ("Сергеев", "Сергей", "Сергеевич", "sergeyadmin", "password3")]
-
-    for admin in added_admins:
-        SuperAdmin.add_admin(*admin)
-
-
-@pytest.fixture(scope="module")
-def fill_db_departments_and_departments_hierarchy():
-    added_department = [
-        ("Финансовый", 101, None),  # id = 1
-        ("Кадровый", 102, 1),  # id = 2
-        ("Информационные Технологии", 103, 1),  # id = 3
-        ("Маркетинг", 104, 2),  # id = 4
-        ("Продажи", 105, 3)  # id = 5
-    ]
-    for department in added_department:
-        SuperAdmin.add_department(*department)
-
-
-@pytest.fixture(scope="module")
-def fill_db_users(create_database, fill_db_departments_and_departments_hierarchy):
-    added_users = [("Иванов", "Иван", "Иванович", "ivanovii", "pass123", 1),
-                   ("Петрова", "Мария", "Алексеевна", "petrovama", "pass234", 2),
-                   ("Сидоров", "Николай", "Петрович", "sidorovnp", "pass345", 3),
-                   ("Кузнецов", "Алексей", "Дмитриевич", "kuznetsovad", "pass456", 4),
-                   ("Смирнова", "Екатерина", "Васильевна", "smirnovaev", "pass567", 5),
-                   ("Волков", "Дмитрий", "Анатольевич", "volkovda", "pass678", 4),
-                   ("Зайцева", "Ольга", "Геннадьевна", "zaytsevog", "pass789", 5)]
-    admin = Administrator()
-    admin.set_self_data({"id": 1, 'name': 'test', 'patronymic': 'test', 'last_name': 'test',
-                         'date_last_changes_password': "10.10.2023"},
-                        "login")  # потому что при добавлении пользователя, используется атрибут id админа
-    for user in added_users:
-        admin.add_user(*user)
-        # last_name=user[0], name=user[1], patronymic=user[2], login=user[3], password=user[4], id_department=user[5]
 
 
 @pytest.fixture(scope="module")

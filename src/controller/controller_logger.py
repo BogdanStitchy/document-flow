3b70b508d92325d47937736f769624b89db644df
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-04-05 13:45:10 +0300

    added the ability to write logs to the database in the controller logger

--- src/controller/controller_logger.py
+++ src/controller/controller_logger.py
@@ -1,5 +1,8 @@
 from loguru import logger
 from src.config.types_role import Role
+from src.db.database_setup import session_factory
+from src.db.models.logs import Logs
+
 
 # Настройка логгера
 controller_logger = logger.bind(client_id="", role="")
@@ -7,8 +10,32 @@ controller_logger.add("logs.log",
                       format="{time} {level} role={extra[role]} client_id={extra[client_id]} method={extra[method]} -> {message}")
 
 
-def set_setting_logger(role: Role, id_client: int):
+def write_log_to_database(message):
+    msg = message.record['message']
+    time = message.record['time']
+    level = message.record['level'].name
+    client_id = message.record['extra']['client_id']
+    role = message.record['extra']['role']
+    method = message.record['extra']['method']
+
+    with session_factory() as session:
+        new_log = Logs(
+            time=time,
+            level=level,
+            client_id=client_id,
+            role=role,
+            method=method,
+            status=msg
+        )
+        session.add(new_log)
+        session.commit()
+
+
+def set_setting_logger(role: Role, id_client: int, database_entry=False):
     global controller_logger
+    if database_entry:
+        controller_logger.add(write_log_to_database,
+                              format="{time} {level} {extra[role]} {extra[client_id]} {extra[method]} -> {message}")
     controller_logger = controller_logger.bind(client_id=str(id_client), role=role)
 
 
